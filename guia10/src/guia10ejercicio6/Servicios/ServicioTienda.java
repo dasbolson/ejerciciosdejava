2719a3dd32b85db5b7447b7d8882193aff153277
Diego Silva <dasbolson@gmail.com>
2023-06-05 22:25:31 -0300

    commit 12 guia 10

--- guia10/src/guia10ejercicio6/Servicios/ServicioTienda.java
+++ guia10/src/guia10ejercicio6/Servicios/ServicioTienda.java
@@ -30,14 +30,73 @@ public class ServicioTienda {
         System.out.println("Cargar codigo del Producto");
         Long codigo = leer.nextLong();
         System.out.println("Precio del Procuto");
-        Double precio= leer.nextDouble();
+        Double precio = leer.nextDouble();
         hashMapProductos.put(codigo, precio);
-        
+
     }
 
-        public void modificarPrecio(HashMap<Long, Double> hashMapProductos) {
-        
-        
+    public void modificarPrecio(HashMap<Long, Double> hashMapProductos) {
+
+        Long codigo;
+        Double precio;
+        System.out.println("Modificar Precio ");
+        System.out.println("------------------");
+        System.out.println("Ingresar Codigo del Producto ");
+        codigo = leer.nextLong();
+
+        if (hashMapProductos.containsKey(codigo)) {
+
+            System.out.println(hashMapProductos.get(codigo));
+            System.out.println("Ingresar nuevo Precio");
+            precio = leer.nextDouble();
+            hashMapProductos.put(codigo, precio);
+            System.out.println(hashMapProductos.get(codigo).toString());
+
+        } else {
+            System.out.println("Codigo Inexistente !!! ");
         }
-        
+
+    }
+
+    // eliminar producto
+    public void eliminarProducto(HashMap<Long, Double> hashMapProductos) {
+        Long codigo;
+
+        System.out.println("Eliminar Producto ");
+        System.out.println("------------------");
+        System.out.println("Ingresar Codigo del Producto ");
+        codigo = leer.nextLong();
+
+        if (hashMapProductos.containsKey(codigo)) {
+            System.out.println("");
+            System.out.println(hashMapProductos.get(codigo));
+            System.out.println("");
+            System.out.println("Eliminando" + hashMapProductos.get(codigo));
+            hashMapProductos.remove(codigo);
+            System.out.println("");
+            System.out.println(hashMapProductos.get(codigo));
+            System.out.println("");
+        } else {
+            System.out.println("Codigo Inexistente !!! ");
+        }
+
+    }
+
+    // Mostrar Productos
+    public void mostrarProductos(HashMap<Long, Double> hashMapProductos) {
+
+        System.out.println(hashMapProductos.toString());
+
+    }
+
+    //genrador automatico
+    public void generadorAutomatico(HashMap<Long, Double> hashMapProductos) {
+       
+        for (int i = 2; i < 10; i++) {
+             
+            hashMapProductos.put(Long.valueOf(i) , Math.random()*10+1);
+
+        }
+
+    }
 }
--- guia10/src/guia10ejercicio6/guia10ejercicio6.java
+++ guia10/src/guia10ejercicio6/guia10ejercicio6.java
@@ -15,14 +15,12 @@ import guia10ejercicio6.Servicios.ServicioTienda;
 import java.util.HashMap;
 import java.util.Scanner;
 
-
-
 public class guia10ejercicio6 {
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
         ServicioTienda sT = new ServicioTienda();
-        HashMap<Long,Double> hashMapProductos=new HashMap();
+        HashMap<Long, Double> hashMapProductos = new HashMap();
         int menu;
 
         do {
@@ -31,29 +29,36 @@ public class guia10ejercicio6 {
             System.out.println("2 Modificar Precio");
             System.out.println("3 Eliminar un Producto");
             System.out.println("4 Mostrar los Productos");
-            System.out.println("5 Salir");
+            System.out.println("5 Generar Automatico");
+            System.out.println("6 Salir");
+
             System.out.println("-------------------------");
             menu = leer.nextInt();
 
             switch (menu) {
                 case 1: // cargar producto
-                        sT.cargarProducto(hashMapProductos);
+                    sT.cargarProducto(hashMapProductos);
                     break;
                 case 2: // modificar precio
-
+                    sT.modificarPrecio(hashMapProductos);
                     break;
                 case 3: // eliminar producto
-
+                    sT.eliminarProducto(hashMapProductos);
                     break;
                 case 4: // mostrar productos
-
+                    sT.mostrarProductos(hashMapProductos);
                     break;
-
+                case 5:
+                    sT.generadorAutomatico(hashMapProductos);
+                    break;
+                case 6: //salir
+                    System.out.println("Chauuu !!!");
+                    break;
                 default:
-
+                    System.out.println("Opcion no valida !");
             }
 
-        } while (menu != 5);
+        } while (menu != 6);
 
     }
 
